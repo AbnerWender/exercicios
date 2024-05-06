4b5e835682932a38e91861c5fda5af41bc40ce31
Abner Wender <138070330+AbnerWender@users.noreply.github.com>
2024-05-06 12:09:13 -0300

    Create exMetroscm.py

---
+++ python/exMetroscm.py
@@ -0,0 +1,5 @@
+''' converter metros para centimetros '''
+print("Converter M para cm")
+metros= float(input("Digite o tamanho em metros: "))
+conversao= metros * 100
+print(metros,"metros é igual a: {0:.2f}".format(conversao),"cm²")
