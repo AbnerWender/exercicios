6a32c67e783d4a3113ed5a92a27d3e85bb654507
Abner Wender <138070330+AbnerWender@users.noreply.github.com>
2024-05-06 12:09:10 -0300

    Create exSomaMulti.py

---
+++ python/exSomaMulti.py
@@ -0,0 +1,7 @@
+'''soma os dois primeiros valores e multiplica pelo terceiro valor'''
+print("soma os 2 primeiros e multiplica pelo 3")
+valor1= float(input("Digite o 1° valor: "))
+valor2= float(input("Digite o 2° valor: "))
+valor3= int(input("Digite o 3° valor: "))
+multi = (valor1+valor2)*valor3
+print("O valor total é = {0:.1f}".format(multi))
