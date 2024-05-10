19edc7012f7342da3da0b7f541dc0c5fdc9517e7
Abner Wender <138070330+AbnerWender@users.noreply.github.com>
2024-05-10 10:48:50 -0300

    aula 10/05

---
+++ python/seletorSexo.py
@@ -0,0 +1,6 @@
+sexo= input("Selecione seu sexo abaixo\n(F) = Feminino (M) = Masculino\nSexo:")
+
+if sexo=='F':
+    print("F - Feminino")
+elif sexo=='M':
+    print("M - Masculino")
---
+++ python/vogalConsoante.py
@@ -0,0 +1,7 @@
+letra= input("Digite uma letra: ")
+
+if letra == "a" or "e"or "i"or "o" or "u":
+    print("A letra é vogal")
+else:
+    print("A letra é consoante")
+    
